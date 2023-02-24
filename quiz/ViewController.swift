040c830e8eef880384366400ecffc98094f141f3
Nikita Sheverdyaev <nikita.sheverdyaev@gmail.com>
2023-02-24 12:03:38 +0300

    feat: Implement basic logic

--- quiz/ViewController.swift
+++ quiz/ViewController.swift
@@ -12,12 +12,25 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    var currentQuestionIndex: Int = 0
+    let questionList: [String] = ["What city are you from", "Whats your girlfriend name", "Whats your dog name"]
+    let answerList: [String] = ["Moscow", "Tasya", "Tima"]
+    
+    
     @IBAction func showNextQuestion(_ sender: UIButton) {
-        
+        currentQuestionIndex = (currentQuestionIndex + 1) % questionList.count
+        questionLabel.text = questionList[currentQuestionIndex]
+        answerLabel.text = "..."
     }
     
     @IBAction func showAnswer(_ sender: UIButton) {
-        
+        answerLabel.text = answerList[currentQuestionIndex]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questionList[currentQuestionIndex]
+        answerLabel.text = "???"
     }
     
     	
