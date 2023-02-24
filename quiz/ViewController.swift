bf04f79096577326d10fbb8c6b30445a6377e027
Nikita Sheverdyaev <nikita.sheverdyaev@gmail.com>
2023-02-24 10:24:36 +0300

    init project

---
+++ quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Nikita on 30.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        
+    }
+    
+    	
+}
+
